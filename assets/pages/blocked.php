7f72ea42860450832f28f6dfc8b1f0cd4878f485
suvadrashrestha <jeenashrestha04@gmail.com>
2024-11-24 23:07:17 +0545

    blocked account content

--- assets/pages/blocked.php
+++ assets/pages/blocked.php
@@ -1,7 +1,7 @@
 <div class="container">
-      <div class="content">
-        <img />
-        <p>hello user you account is blocked</p>
-        <a href="assets/php/actions.php?logout">logout</a>
-      </div>
-    </div>
+  <div class="content">
+    <img />
+    <!p>hello user you account is blocked !</!p>
+    <a href="assets/php/actions.php?logout">logout</a>
+  </div>
+</div>
